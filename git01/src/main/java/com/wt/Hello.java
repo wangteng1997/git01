30d5488f8f75f7a1b9e2bfe49e82f98675d453c8
wangteng1997 <1124256537@qq.com>
2020-09-09 16:51:43 +0800

    Revert "Revert "Revert "Revert "创建了一个项目！~""""
    This reverts commit 520b2d5d

--- git01/src/main/java/com/wt/Hello.java
+++ git01/src/main/java/com/wt/Hello.java
@@ -10,6 +10,5 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("12345");
         System.out.println("baby");
-        System.out.println("你好吗？");
     }
 }
